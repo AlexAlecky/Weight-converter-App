56617ede9bcf64e130c24132907cb04a96a0928e
Alex Maina <aleckysanchez47@gmail.com>
2023-12-11 14:39:25 +0300

    First commit - committing all files to the repasitory

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputEl = document.getElementById('input');
+const errorEl = document.getElementById('error');
+const resultEl = document.getElementById('result');
+
+let errorTime
+let resultTime
+
+function updateResults() {
+  if(inputEl.value <= 0 || isNaN(inputEl.value)) {
+    errorEl.innerHTML = 'Please Enter a valid number'
+    setTimeout(()=>{
+      clearTimeout = errorTime
+      errorEl.innerHTML = '';
+      inputEl.value = '';
+    }, 2000)
+  } else {
+    resultEl.innerHTML = +(inputEl.value / 2.2).toFixed(2);
+    setTimeout(()=>{
+      clearTimeout = resultTime
+      resultEl.innerHTML = '';
+      inputEl.value = '';
+    }, 2000)
+  }
+}
+
+
+inputEl.addEventListener('input', updateResults);
